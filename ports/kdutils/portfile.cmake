c772907262cb831865f95304c841c11982806ce8
Paul Lemire <paul.lemire@kdab.com>
2025-10-21 14:44:53 +0200

    Update kdutils portfile.cmake

--- ports/kdutils/portfile.cmake
+++ ports/kdutils/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KDAB/KDUtils
-    REF a1458532372bf82a6ee97c95dbdea7c5c2d8dc6a
-    SHA512 128dee0c7346a254b6b8b068d1005670dc10b104965634af5b65479c1ed499d471d99006bda73802da64174326baaaffcaa2d9894391ef65a8b85d5a65bf96e0
+    REF 25689bf03f1559e28cdda651625a08c51305afe4
+    SHA512 ae0856f77f1ff3cd487bb1008160452b06f08b41b3928fd7090789f5e880c6aca73deeb6f38c23a6e4fd480be122e37270ccf068f91820b2a4ea76857b841abb
     HEAD_REF main
 )
 
@@ -17,4 +17,23 @@ vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
                       -DKDUTILS_BUILD_EXAMPLES=OFF
                       ${FEATURE_OPTIONS})
 vcpkg_cmake_install()
+
+# Remove debug includes if they exist
+if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/include")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+endif()
+
+# Fix CMake config files - each module goes to its own subdirectory in share/
+if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/cmake")
+    vcpkg_cmake_config_fixup(PACKAGE_NAME KDUtils CONFIG_PATH lib/cmake/KDUtils DO_NOT_DELETE_PARENT_CONFIG_PATH)
+    vcpkg_cmake_config_fixup(PACKAGE_NAME KDGui CONFIG_PATH lib/cmake/KDGui DO_NOT_DELETE_PARENT_CONFIG_PATH)
+    vcpkg_cmake_config_fixup(PACKAGE_NAME KDFoundation CONFIG_PATH lib/cmake/KDFoundation)
+endif()
+
+# Copy usage file if it exists
+if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/usage")
+    file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+endif()
+
+# Copy License file
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
